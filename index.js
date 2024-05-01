d20e4e5aa53ecd64a6acb2623c3c1e1f033a2968
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-05-01 19:28:16 -0300

    hora atual

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const localStorageKey = 'to-do-list-fo';
 const list = document.getElementById('list')
 
+function mostrarHora(){
+    let horaAtual = new Date().getHours();
+    let minutoAtual = new Date().getMinutes();
+    let segundosAtual = new Date().getSeconds();
+    let hora = document.getElementById('hora');
+    hora.innerText = `${horaAtual} : ${minutoAtual} : ${segundosAtual}`
+    setInterval(mostrarHora, 1000);
+}
+mostrarHora()
+
 function adicionar(){
     let task = document.getElementById('task').value;
     //validação de tarefa adicionada
@@ -17,15 +27,16 @@ function adicionar(){
     }
 }   
 
+//função para mostrar valores na tela
 function showValues(){
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
     list.innerHTML ='';
     for(i = 0;i < values.length; i++){
-        list.innerHTML += `<li>${values[i]['name']} <button id = "btn-ok" onclick="removeItem('${values[i]['name']}')">ok</button></li>`
+        list.innerHTML += `<li>${values[i]['name']} <button id = "btn-ok" onclick="removeItem('${values[i]['name']}')">\u{2705}</button></li>`
     }
     console.log(values)
 }
-
+//remoção de dado direto no locaStorage
 function removeItem(data){
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     let index = values.findIndex(x => x.name == data);
